7259db636b0b5e1c5fe128823f813a9360766949
rafalBaron <rafal.it@baron.hub.pl>
2024-11-11 20:27:29 +0100

    Typography

--- app/src/main/java/com/TraSka/ui/theme/Type.kt
+++ app/src/main/java/com/TraSka/ui/theme/Type.kt
@@ -2,33 +2,33 @@ package com.TraSka.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.TraSka.R
 
-// Set of Material typography styles to start with
+val RobotoFontFamily = FontFamily(
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_italic, FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+    Font(R.font.roboto_bolditalic, FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_lightitalic, FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_mediumitalic, FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.roboto_black, FontWeight.Black),
+    Font(R.font.roboto_blackitalic, FontWeight.Black, style = FontStyle.Italic),
+    Font(R.font.roboto_thin, FontWeight.Thin),
+    Font(R.font.roboto_thinitalic, FontWeight.Thin, style = FontStyle.Italic)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
+        fontFamily = RobotoFontFamily,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
